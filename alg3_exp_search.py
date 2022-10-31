17ab2a0e0c8a9c8e7d4d0525fd7c38386ec77d3b
Bathris <bathris12@gmail.com>
2022-10-31 03:15:12 +0300

    update

--- alg3_exp_search.py
+++ alg3_exp_search.py
@@ -1,6 +1,3 @@
-import time
-
-
 def bin_search_in_col(matrix, target, col, left, right):
     while left + 1 < right:
         mid = (right + left) // 2
@@ -14,10 +11,11 @@ def exp_search_in_col(matrix, target, col, start):
     border = 1
     while start + border < len(matrix) and matrix[start+border][col] < target:
         border = border * 2
-    #if matrix[min(start+border, len(matrix)-1)][col] < target: return min(start+border, len(matrix)-1)  # TODO: проверить верность
     return bin_search_in_col(matrix, target, col, left=start+border//2-1, right=min(start+border, len(matrix)-1))
 
 def bin_search_in_row(matrix, target, row, left, right):
+    if target < matrix[row][left]: return left
+    elif target > matrix[row][right]: return right
     while left + 1 < right:
         mid = (right + left) // 2
         if matrix[row][mid] <= target:
@@ -27,36 +25,27 @@ def bin_search_in_row(matrix, target, row, left, right):
     return left
 
 def exp_search_in_row(matrix, target, row, start):
+    if matrix[row][0] >= target:
+        return 0
     border = 1
     while (start - border) > 0 and matrix[row][start-border] > target:
-        border = border * 2
-    t = bin_search_in_row(matrix, target, row, left=max(-1, start-border-1), right=start-border//2)
+        border *= 2
+    t = bin_search_in_row(matrix, target, row, left=max(0, start-border), right=start-border//2)
     return t
 
 
 def exp_search(matrix, target):
     row_it = 0
     col_it = len(matrix[0])-1
-    while matrix[row_it][col_it] != target and (row_it <= len(matrix)-1 and col_it >= 0):
+    matrix_length = len(matrix)
+    while matrix[row_it][col_it] != target and (row_it <= matrix_length-1 and col_it >= 0):
         if matrix[row_it][col_it] > target:
             if col_it == 0: break
-            #print(f"CHANGING COL ({row_it}, {col_it})")
             col_it = exp_search_in_row(matrix, target, row_it, start=col_it)
-            #print("COL CHANGED:", col_it, row_it)
         else:
-            if row_it == len(matrix) - 1: break
-            #print(f"CHANGING ROW ({row_it}, {col_it})")
+            if row_it == matrix_length - 1: break
             row_it += 1
-            #print("ROW CHANGED:", col_it, row_it)
     if matrix[row_it][col_it] == target:
         return row_it, col_it
     else:
         return -1, -1
-
-if __name__ == '__main__':
-    target = 5
-    a = [[0,1,2,3,4],
-         [4,7,8,9,10],
-         [5,7,8,11,12],
-         [7,9,12,15,20]]
-    print(exp_search(a, target))
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import gc
 import random
 import time
+import timeit
 
 from alg1_bin_search import bin_search
 from alg2_linear_search import linear_search
@@ -22,18 +24,34 @@ def print_matrix(matrix):
         print()
 
 def timeit_and_exec(func, args: list = list()):
+    gc.disable()
     t = time.time_ns()
-    ans = func(*args)
+    ans = func(args[0], args[1])
     t2 = time.time_ns() - t
+    gc.enable()
     return {"execution_time": t2,
             "function": func.__name__,
             "answer": ans}
 
-def get_median_execution_time_of_function(func, args, repeat=10):
+def timeit_and_exec_v2(func, args: list = list()):
+    c = globals().copy()
+    c['args'] = args
+    t = timeit.timeit(f"{func.__name__}(*args)", globals=c)
+    print(t)
+    return {"execution_time": t,
+            "function": func.__name__}
+
+
+def get_median_execution_time_of_function(func, args, repeat=150):
     s = 0
-    for i in range(10):
-        s += timeit_and_exec(func, args)['execution_time']
-    return s//repeat
+    d = 0
+    for i in range(repeat):
+        t = timeit_and_exec(func, args)['execution_time']
+        if t != 0:
+            s += t
+            d += 1
+    if d == 0: d = 1
+    return s//d
 
 def get_median_search_time_in_matrix_by_algorithms(matrix_size, matrix_type=1):
     N = 2 ** matrix_size
@@ -56,7 +74,7 @@ def check_correctness_of_algorithms():
     matrix = create_matrix(matrix_size, fill_type=matrix_type)
     print(len(matrix), len(matrix[0]))
     while True:
-        target = random.choice(random.choice(matrix[12:]))+random.randint(0, 1)
+        target = random.choice(random.choice(matrix[12:]))+random.randint(0, 1)+1
         bin_search_ans = timeit_and_exec(bin_search, [matrix, target])['answer']
         linear_search_ans = timeit_and_exec(linear_search, [matrix, target])['answer']
         exp_search_ans = timeit_and_exec(exp_search, [matrix, target])['answer']
@@ -79,5 +97,6 @@ if __name__ == '__main__':
               f"  EXP2 SEARCH: {timings2['exp_search_time']}\n")
         s += f"2^{i}\t{timings['bin_search_time']}\t{timings['linear_search_time']}\t{timings['exp_search_time']}\t{timings2['exp_search_time']}\t"
         if float(timings2['exp_search_time']) == 0: s += "0" + "\n"
+        else: s += "\n"
     f.write(s.replace(".", ","))
     f.close()
